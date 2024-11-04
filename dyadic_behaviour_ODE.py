c6e697b469966acdf0eebafe9449654dfa8966ed
jimallenglasgow <143419212+jimallenglasgow@users.noreply.github.com>
2024-11-04 19:43:35 +0000

    feat - many equations

--- dyadic_behaviour_ODE.py
+++ dyadic_behaviour_ODE.py
@@ -25,38 +25,74 @@ from scipy.integrate import solve_ivp
 
 def Behaviour_Model_ODE(t, b):#, alpha, beta, gamma, delta, epsilon):
 
-	for sel_ind in np.arange(2):
+	for sel_ind in np.arange(no_eqs):
 	
+		if b[sel_ind]>1000:
+		
+			b[sel_ind]=1000
+			
+		if b[sel_ind]<-1000:
+		
+			b[sel_ind]=-1000
+			
+	for sel_ind in np.arange(2):
+			
 		if b[sel_ind]<0:
 		
 			b[sel_ind]=0
 
-	b_dot=np.zeros(2)
+	b_dot=np.zeros(no_eqs)
 
-	for sel_ind in np.arange(2):
+	for sel_ind in np.arange(no_eqs):
 
-		b_dot[sel_ind]=alpha[sel_ind]*(b[sel_ind])-beta[sel_ind]*(b[sel_ind]**2)+gamma[sel_ind]*b[1-sel_ind]+epsilon[sel_ind]-delta[sel_ind]*(b[sel_ind]+b[1-sel_ind])
+#		b_dot[sel_ind]=b[sel_ind]*(alpha[sel_ind]*(b[sel_ind])-beta[sel_ind]*(b[sel_ind]**2)+gamma[sel_ind]*b[1-sel_ind]+epsilon[sel_ind]-delta[sel_ind]*(b[sel_ind]+b[1-sel_ind]))+(np.random.random()*2-1)*0.1
+		
+		#b_dot[sel_ind]=b[sel_ind]*(delta[sel_ind]-alpha[sel_ind]*(b[sel_ind]**2)-gamma[sel_ind]*b[1-sel_ind])+(np.random.random()*2-1)*0.1
+		
+		#b_dot[sel_ind]=b[sel_ind]*(5-b[sel_ind])*(alpha[sel_ind]*b[sel_ind]+beta[sel_ind]*b[1-sel_ind]+delta[sel_ind])+(np.random.random()*2-1)*0.2
+		
+		b_dot_tmp=alpha[sel_ind]*b[sel_ind]
+		
+		for sel_other_ind in np.arange(no_eqs):
+		
+			b_dot_tmp=b_dot_tmp+beta[sel_ind, sel_other_ind]*b[sel_other_ind]
+			
+#		if sel_ind<2:
+		
+		b_dot[sel_ind]=(b[sel_ind]-epsilon[sel_ind])*(delta[sel_ind]-b[sel_ind])*b_dot_tmp#+(np.random.random()*2-1)*0.2
+			
+#		else:
+		
+#			b_dot[sel_ind]=b_dot_tmp
 		
 	return(b_dot)
 	
+no_eqs=5
 
-alpha=np.random.random(2)*2
+alpha=np.random.random(no_eqs)*2-1
 
 print("alpha = ",alpha)
 
-beta=np.random.random(2)*2
+beta=np.random.random([no_eqs,no_eqs])*2-1
+
+for i in np.arange(no_eqs):
+
+	beta[i,i]=0
 
 print("beta = ",beta)
 
-gamma=np.random.random(2)*2
+gamma=np.random.random(no_eqs)*2-1
 
 print("gamma = ",gamma)
 
-delta=np.random.random(2)*2
+delta=np.random.random(no_eqs)*7
 
 print("delta = ",delta)
 
-epsilon=np.random.random(2)*2
+epsilon=np.random.random(no_eqs)*2-1
+
+epsilon[0]=0
+epsilon[1]=0
 
 print("epsilon = ",epsilon)
 
@@ -66,24 +102,34 @@ print("epsilon = ",epsilon)
 		
 #print("b_dot = ",b_dot)
 
-b_init=np.random.random(2)*3
+b_init=np.random.random(no_eqs)*0.4
 	
 t_max=0
 
-no_t=100
+no_t=250
 
-full_z=np.empty([2, 1])
+full_z=np.reshape(b_init,(no_eqs,1))
 
-full_t=np.empty(1)
+print(full_z)
+
+#full_t=[]#np.empty(shape=[1])
+
+full_t=[0]
+
+print(full_t)
 
 nudge_behaviour=0
 
-for i in np.arange(5):
+for i in np.arange(3):
 
-	#b_init[0]=b_init[0]+nudge_behaviour*np.random.random()*3
+	b_init[[0,1]]=b_init[[0,1]]+nudge_behaviour*0.5#np.random.random(2)*2
 
-	alpha[1]=alpha[1]+nudge_behaviour*np.random.random()*3
-
+#	alpha[[0,1]]=alpha[[0,1]]+nudge_behaviour*0.5#(np.random.random(2)*2)*0.5
+	
+#	beta=beta+nudge_behaviour*0.5#(np.random.random(2)*2)*0.5
+	
+#	delta[[0,1]]=delta[[0,1]]+nudge_behaviour*(np.random.random(2)*2)*0.5
+	
 	t_min=t_max
 
 	t_max=t_max+10
@@ -93,11 +139,11 @@ for i in np.arange(5):
 	sol=solve_ivp(Behaviour_Model_ODE, [np.min(t_sol), np.max(t_sol)], b_init, dense_output=True, t_eval=t_sol)
 
 	z=sol.sol(t_sol)
-
-	full_z=np.hstack([full_z, z])
-
-	full_t=np.hstack([full_t, t_sol])
-
+	
+	full_z=np.hstack([full_z,z])
+		
+	full_t=np.hstack([full_t,t_sol])
+	
 	L=len(z[0,:])
 
 	b_init=z[:,L-1]
@@ -106,11 +152,13 @@ for i in np.arange(5):
 
 	fig, ax = plt.subplots(nrows=1, ncols=2)
 
-	ax[0].plot(full_t, full_z.T)
+	ax[0].plot(full_t, full_z.T[:,[0,1]])
 	#plt.ylim([0, 20])
 	#ax[0].x_label('t')
 
-	ax[1].plot(full_z.T[:,0],full_z.T[:,1])
+#	ax[1].plot(full_z.T[:,0],full_z.T[:,1])
+	
+	ax[1].plot(full_t, full_z.T[:,np.arange(2,no_eqs)])
 
 	plt.show()
 	plt.close()

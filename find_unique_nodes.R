102d632c0f4b3096d23c5fc472e23da8b9c20cb5
jimallenglasgow <143419212+jimallenglasgow@users.noreply.github.com>
2025-11-29 17:48:26 +0000

    Network checks
    Network checks

---
+++ find_unique_nodes.R
@@ -0,0 +1,34 @@
+##code to find the unique nodes in the network, and also check in degree and out degree
+
+source("libraries_etc.R")
+
+####################################################################################
+
+##load in the network
+
+ds_network<-data.table(read.csv("pa_ds_network.csv"))
+
+##calculate the in degree and out degree so we can check it with the data in the paper (check on page 81 of the paper)
+
+out_degree<-ds_network[,.(out_degree=.N), by=From]
+in_degree<-ds_network[,.(in_degree=.N), by=To]
+
+names(out_degree)<-c("Node", "Out degree")
+names(in_degree)<-c("Node", "In degree")
+
+all_degrees<-merge(in_degree, out_degree, all = T)
+
+all_degrees[is.na(all_degrees)]<-0
+
+
+
+
+
+
+
+
+
+
+
+
+
--- libraries_etc.R
+++ libraries_etc.R
@@ -1,3 +1,6 @@
 rm(list = ls())
 library(tidyverse)
 library(data.table)
+library(rstudioapi)
+
+setwd(dirname(getActiveDocumentContext()$path))   
